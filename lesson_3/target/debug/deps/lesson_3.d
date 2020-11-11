712b1b523f0e30de66cadb791ef1b5db5db832b2
Dario Lencina <dario@armore.dev>
2020-11-11 07:32:18 -0500

    save

--- lesson_3/target/debug/deps/lesson_3.d
+++
@@ -1,5 +0,0 @@
-/Users/darioalessandro/Documents/2020/vim/lesson_3/target/debug/deps/lesson_3: src/main.rs
-
-/Users/darioalessandro/Documents/2020/vim/lesson_3/target/debug/deps/lesson_3.d: src/main.rs
-
-src/main.rs:
--- lesson_3/target/debug/lesson_3.d
+++
@@ -1,1 +0,0 @@
-/Users/darioalessandro/Documents/2020/vim/lesson_3/target/debug/lesson_3: /Users/darioalessandro/Documents/2020/vim/lesson_3/src/main.rs
